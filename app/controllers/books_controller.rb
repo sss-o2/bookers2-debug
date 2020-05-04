0f9d0ab7fbeb8af7516e7148677cda5126bc7b5a
名前 <>
2020-05-04 14:18:14 +0900

    [Finish]ouyoukadai2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,57 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :baria_user, only: [:update,:edit]
+
+  def show
+    @book = Book.find(params[:id])
+    @user= User.new(@current_user)
+    @booknew=Book.new
+  end
+
+  def index
+    @books = Book.all 
+    @booknew=Book.new
+  end
+
+  def create
+    @booknew = Book.new(book_params) 
+    @booknew.user_id=current_user.id
+    if @booknew.save 
+      redirect_to @booknew, notice: "You have creatad book successfully."
+    else
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to @book, notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path, notice: "successfully delete book!"
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
+  def baria_user
+    unless Book.find(params[:id]).user_id == current_user.id
+      redirect_to books_path
+    end
+   end
+
+end
